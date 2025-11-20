0ddb1222c675b46dc155c2b66e636f70d23350a5
FeX <bdfernando352@gmail.com>
2025-11-20 06:32:53 -0600

    footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,18 @@
     </main>
-    <footer class="pie-pagina">
+    <footer class="pie-pagina" aria-label="Pie de página">
         <div class="contenedor">
-            <div class="derechos-autor">
-                &copy; <?php echo date('Y'); ?> Los Tralaleros. Todos los derechos reservados.
+            <div class="derechos-autor" aria-label="Información de derechos de autor">
+                &copy; <?php echo date('Y'); ?> Los Tralaleros. Todos los derechos reservados. <br>
+
+                Lucas Perez Angel Rodolfo      <br>
+                Martinez Mendoza Andrick Jesus <br>
+                Pineda Vargas Luis Antonio     <br>
+                Morales Enriquez Marco Antonio <br>
+                Rojas Gomez Erick Alberto      <br>
+                Valencia Orozco Luis Fernando
             </div>
         </div>
     </footer>
     
-    <link rel="stylesheet" href="css/footer.css">
 </body>
 </html>
